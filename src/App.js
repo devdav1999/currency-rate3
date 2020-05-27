16a1cd8f62d2dff1f1556056894065c2e6c6d567
Jensen David <feudavid92@gmail.com>
2020-05-26 19:52:46 -0700

    got main bars to appear

--- src/App.js
+++ src/App.js
@@ -7,27 +7,24 @@ function App() {
   const [mainCurrencies, setMainCurrencies] = useState(["HKD", "USD", "AUD", "GBP", "CAD"]);
   const [countries, setCountries] = useState('');
   const [numbers, setNumbers] = useState('');
-  const [rates, setRates] = useState('');
+  const [rates, setRates] = useState([]);
   
 
   function doFetch() {
     const url = "https://api.exchangeratesapi.io/latest";
-    const currencyData = [];
     fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log("the data", data);
             console.log(data.rates);
-            currencyData.push(data.rates);
-            console.log(currencyData);
             // this.setState({
             //   countries: Object.keys(currencyData),
             //   numbers: Object.values(currencyData),
             // });
-            setRates(currencyData);
+            setRates(Object.entries(data.rates));
             setMainCurrencies(mainCurrencies);
-            setCountries(Object.keys(currencyData));
-            setNumbers(Object.values(currencyData));
+            // setCountries(Object.keys(currencyData));
+            // setNumbers(Object.values(currencyData));
         });
   }
 
@@ -45,11 +42,12 @@ function App() {
       
       <div className="BarContainer">
       {
-        mainCurrencies
-        .map(mainCurrency =>(
-        <div className='Bar'>
-        {mainCurrency}
-        </div>
+        rates
+        .filter(rate => mainCurrencies.includes(rate[0]))
+        .map(rate =>(
+            <div className='Bar' style={{height: (1/rate[1] * 100) + '%'}}>
+            {rate[0]}
+            </div>
         ))
         }
       </div>
